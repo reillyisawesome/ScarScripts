a2a300ba0d9c6796de2f751065903fa107cc428a
reillyisawesome <justinproductionz@gmail.com>
2015-11-12 22:14:54 -0600

    test
    test

---
+++ test.java
@@ -0,0 +1,18 @@
+package scripts;
+ 
+import org.tribot.script.Script;
+import org.tribot.script.ScriptManifest;
+ 
+@ScriptManifest(authors = { "Boolean" }, category = "Woodcutting", name = "woodcutter")
+public class test extends Script{
+ 
+ 
+ 
+@Override
+public void run() {
+ 
+println("hello world");
+}
+ 
+ 
+}
