c61a74bdc75f66587edf818428ea1d859452509a
Justin Reilly <justinproductionz@gmail.com>
2015-11-13 21:32:45 -0600

    f
    f

--- test.java
+++ test.java
@@ -1,18 +1,15 @@
 package scripts;
- 
+
 import org.tribot.script.Script;
 import org.tribot.script.ScriptManifest;
  
 @ScriptManifest(authors = { "Boolean" }, category = "Woodcutting", name = "woodcutter")
 public class test extends Script{
- 
- 
- 
 @Override
 public void run() {
  
-println("hello world");
+println("helos rld");
 }
- 
+
  
 }
